b523716240e3c4359d7d6d8e8c86f9bfd1975156
mikeqiu2011 <mikeqiu2011@163.com>
2019-08-31 15:12:08 +0800

    inner class added and various test cases added

--- src/test/java/com/realestateapp/ApartmentRaterTest.java
+++ src/test/java/com/realestateapp/ApartmentRaterTest.java
@@ -1,18 +1,23 @@
 package com.realestateapp;
 
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ApartmentRaterTest {
+    private RandomApartmentGenerator randomApartmentGenerator;
 
     @BeforeEach
     void setUp() {
+        this.randomApartmentGenerator = new RandomApartmentGenerator();
     }
 
     @AfterEach
@@ -21,10 +26,11 @@ class ApartmentRaterTest {
 
     @Nested
     class RateApartmentTests {
-        @Test
-        void should_ReturnZero_when_RateAsCheap(){
+        @ParameterizedTest
+        @CsvFileSource(resources = "/cheap_departments.csv",numLinesToSkip = 1)
+        void should_ReturnZero_when_RateAsCheap(double area, BigDecimal price){
             //given
-            Apartment apartment = new Apartment(100,new BigDecimal(3000000));
+            Apartment apartment = new Apartment(area,price);
 
             //when
             int actual = ApartmentRater.rateApartment(apartment);
@@ -32,9 +38,96 @@ class ApartmentRaterTest {
             //then
             assertEquals(0, actual);
         }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "/moderate_departments.csv",numLinesToSkip = 1)
+        void should_ReturnOne_when_RateAsModerate(double area, BigDecimal price){
+            //given
+            Apartment apartment = new Apartment(area,price);
+
+            //when
+            int actual = ApartmentRater.rateApartment(apartment);
+
+            //then
+            assertEquals(1, actual);
+        }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "/expensive_departments.csv",numLinesToSkip = 1)
+        void should_ReturnTwo_when_RateAsExpensive(double area, BigDecimal price){
+            //given
+            Apartment apartment = new Apartment(area,price);
+
+            //when
+            int actual = ApartmentRater.rateApartment(apartment);
+
+            //then
+            assertEquals(2, actual);
+        }
+
+        @Test
+        void should_ReturnMinusOne_when_AreaIsZero(){
+            //given
+            Apartment apartment = new Apartment(0,new BigDecimal(100));
+
+            //when
+            int actual = ApartmentRater.rateApartment(apartment);
+
+            //then
+            assertEquals(-1, actual);
+        }
+
     }
 
     @Nested
     class CalculateAverageRatingTests {
+
+        @Test
+        void should_ThrowRunTimeException_when_ListIsEmpty(){
+            //given
+            List<Apartment> apartments = new ArrayList<Apartment>();
+
+            //when
+            Executable executable = () -> ApartmentRater.calculateAverageRating(apartments);
+
+            //then
+            assertThrows(RuntimeException.class, executable);
+        }
+
+        @Test
+        void should_ReturnReasonableAveragePrice_when_ListIsNotEmpty(){
+            //given
+            List<Apartment> apartments = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                apartments.add(ApartmentRaterTest.this.randomApartmentGenerator.generate());
+            }
+
+            //when
+            double actual = ApartmentRater.calculateAverageRating(apartments);
+//            System.out.println("actual: " + actual);
+
+            //then
+            assertAll(
+                    () -> assertTrue(actual > 0),
+                    () -> assertTrue(actual < 2)
+            );
+
+        }
+
+        @Test
+        void should_ReturnRunWithIn500Ms_when_1000ListItemsIsGiven(){
+            //given
+            List<Apartment> apartments = new ArrayList<>();
+            for (int i = 0; i < 1000; i++) {
+                apartments.add(ApartmentRaterTest.this.randomApartmentGenerator.generate());
+            }
+
+            //when
+            Executable executable = () -> ApartmentRater.calculateAverageRating(apartments);
+
+            assertTimeout(Duration.ofMillis(500), executable);
+
+        }
+
     }
 }
